af7952af5cfdcfee9120e0bacbd8e1d7a15fa8b7
Isaac Bunga <isaac.bunga@ideiasdinamicas.com>
2024-09-18 17:10:26 +0100

    feat:update data and add tables

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,21 +1,32 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';  
+import { Component, inject, OnInit } from '@angular/core';  
+import { MatTableModule } from '@angular/material/table';  
 
-@Component({
-  selector: 'app-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
-})
-export class ListComponent {
-  products :any[]=[]
- httpClient = inject(HttpClient)
+export interface Produto {  
+  id: number;  
+  nome: string;  
+  descricao: string;  
+  preco: number;  
+  categorias: string[];  
+  estoque: number;  
+  imagem: string;  
+}  
 
- ngOnInit(): void {
-  this.httpClient.get<any>('/api/store').subscribe((products)=>{
- this.products =products.produts
-  })
+@Component({  
+  selector: 'app-list',  
+  standalone: true,  
+  imports: [MatTableModule],  
+  templateUrl: './list.component.html',  
+  styleUrls: ['./list.component.scss']  
+})  
+export class ListComponent implements OnInit {  
+  displayedColumns: string[] = ['id', 'nome', 'descricao', 'preco', 'estoque', 'imagem'];  
+  products: Produto[] = [];  
+  httpClient = inject(HttpClient);  
 
- }
+  ngOnInit(): void {  
+    this.httpClient.get<Produto[]>('/api/produts').subscribe((products) => {  
+      this.products = products;  
+    });  
+  }  
 }
